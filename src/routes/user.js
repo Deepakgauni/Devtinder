8f24295a98b909708cb477a9ee6714c97d0d8470
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2026-04-26 13:46:19 +0530

    build a feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express")
 const userRouter= express.Router()
 const {userauth}= require("../middlewares/auth")
 const ConnectionRequestModel= require("../models/connectionRequest")
+const User = require("../models/user")
+const USER_SAFE_DATA = "firstName lastName email ProfileUrl"
 
 userRouter.get("/user/request/received", userauth, async(req, res)=>{
     try{
@@ -42,6 +44,43 @@ userRouter.get("/user/connection", userauth, async(req, res)=>{
         res.json({data})
 
     }catch(error){
+        res.status(400).json({message: error.message})
+    }
+})
+
+userRouter.get("/feed", userauth, async(req, res)=>{
+    try{
+        const loggedInUser= req.user;
+        
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 20 ? 20 : limit;
+
+
+        const skip = (page-1)*limit;
+
+
+        const connectionsRequests = await ConnectionRequestModel.find({
+            $or:[{fromUserId: loggedInUser._id, }, {toUserId: loggedInUser._id}],
+        }).select("fromUserId toUserId " )
+        
+
+        const hideUsersFromFeed =  new Set()
+        connectionsRequests.forEach((req)=>{
+            hideUsersFromFeed.add(req.fromUserId.toString())
+            hideUsersFromFeed.add(req.toUserId.toString())
+        })
+
+        const users = await User.find({
+            $and:[
+                {_id: {$nin: Array.from(hideUsersFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit)
+
+         res.send(users)
+    }
+    catch(error){
         res.status(400).json({message: error.message})
     }
 })
